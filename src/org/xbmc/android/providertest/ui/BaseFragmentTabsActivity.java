8c2904dfce8c5470f30de398ac71f96ca183f510
freezy <freezy@xbmc.org>
2011-09-14 00:35:14 +0200

    Added: Glued tab activity together with Google I/O's BaseActivity.

---
+++ src/org/xbmc/android/providertest/ui/BaseFragmentTabsActivity.java
@@ -0,0 +1,90 @@
+package org.xbmc.android.providertest.ui;
+
+import org.xbmc.android.providertest.R;
+import org.xbmc.android.util.TabsAdapter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
+
+/**
+ * A {@link BaseActivity} that simply contains a single fragment. The intent
+ * used to invoke this activity is forwarded to the fragment as arguments during
+ * fragment instantiation. Derived activities should only need to implement
+ * {@link com.google.android.apps.iosched.ui.BaseSinglePaneActivity#onCreatePane()}
+ * .
+ */
+public abstract class BaseFragmentTabsActivity extends BaseActivity {
+	
+	private TabHost mTabHost;
+	private TabWidget mTabWidget;
+	private TabsAdapter mTabsAdapter;
+	private ViewPager mViewPager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fragment_tabs_pager);
+		
+		final HorizontalScrollView scroller = (HorizontalScrollView) findViewById(R.id.tab_scroller);
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+		mTabWidget = (TabWidget) findViewById(android.R.id.tabs);
+		mViewPager = (ViewPager) findViewById(R.id.tab_pager);
+
+		mTabHost.setup();
+		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
+		
+		getActivityHelper().setupActionBar(getTitle(), 0);
+
+		final String customTitle = getIntent().getStringExtra(Intent.EXTRA_TITLE);
+		getActivityHelper().setActionBarTitle(customTitle != null ? customTitle : getTitle());
+
+		if (savedInstanceState == null) {
+			onCreateTabs();
+			//mFragment = onCreatePane();
+			//mFragment.setArguments(intentToFragmentArguments(getIntent()));
+
+		} else {
+			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
+		}
+		
+		mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+			@Override
+			public void onTabChanged(String tabId) {
+				final int position = mTabHost.getCurrentTab();
+				final View tab = mTabWidget.getChildTabViewAt(position);
+				scroller.smoothScrollTo(tab.getLeft(), 0);
+				mViewPager.setCurrentItem(position);
+			}
+		});
+	}
+	
+	protected void addTab(String key, String label, Class<?> fragment, int imageResource) {
+		final View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab, mTabWidget, false);
+		final TextView title = (TextView) tabIndicator.findViewById(R.id.tab_title);
+		final ImageView icon = (ImageView) tabIndicator.findViewById(R.id.tab_icon);
+		title.setText(label);
+		icon.setImageResource(imageResource);
+		mTabsAdapter.addTab(mTabHost.newTabSpec(key).setIndicator(tabIndicator), label, fragment, null);
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("tab", mTabHost.getCurrentTabTag());
+	}
+
+	/**
+	 * Called in <code>onCreate</code> when the fragment constituting this
+	 * activity is needed. The returned fragment's arguments will be set to the
+	 * intent used to invoke this activity.
+	 */
+	protected abstract void onCreateTabs();
+}
--- src/org/xbmc/android/providertest/ui/MusicPagerActivity.java
+++ src/org/xbmc/android/providertest/ui/MusicPagerActivity.java
@@ -1,70 +1,13 @@
 package org.xbmc.android.providertest.ui;
 
 import org.xbmc.android.providertest.R;
-import org.xbmc.android.util.TabsAdapter;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
-import android.widget.TabHost;
-import android.widget.TabWidget;
-import android.widget.TextView;
-
-public class MusicPagerActivity extends FragmentActivity {
-	TabHost mTabHost;
-	ViewPager mViewPager;
-	TabsAdapter mTabsAdapter;
+public class MusicPagerActivity extends BaseFragmentTabsActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.fragment_tabs_pager);
-		
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-		final HorizontalScrollView scroller = (HorizontalScrollView) findViewById(R.id.tab_scroller);
-		final TabWidget tabWidget = (TabWidget) findViewById(android.R.id.tabs);
-		mTabHost.setup();
-
-		mViewPager = (ViewPager) findViewById(R.id.tab_pager);
-		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
-
-		addTab(tabWidget, "albums", "Albums", AlbumsFragment.class, R.drawable.tab_ic_album);
-		addTab(tabWidget, "artists", "Artists", AlbumsFragment.class, R.drawable.tab_ic_artist);
-		addTab(tabWidget, "files", "Files", AlbumsFragment.class, R.drawable.tab_ic_folder);
-		
-		
-		mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-			@Override
-			public void onTabChanged(String tabId) {
-				final int position = mTabHost.getCurrentTab();
-				final View tab = tabWidget.getChildTabViewAt(position);
-				scroller.smoothScrollTo(tab.getLeft(), 0);
-				mViewPager.setCurrentItem(position);
-			}
-		});
-
-		if (savedInstanceState != null) {
-			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
-		}
-	}
-	
-	private void addTab(TabWidget tabWidget, String tabspec, String label, Class<?> c, int imgRes) {
-		final View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab, tabWidget, false);
-		final TextView title = (TextView) tabIndicator.findViewById(R.id.tab_title);
-		final ImageView icon = (ImageView) tabIndicator.findViewById(R.id.tab_icon);
-		title.setText(label);
-		icon.setImageResource(imgRes);
-		mTabsAdapter.addTab(mTabHost.newTabSpec(tabspec).setIndicator(tabIndicator), label, c, null);
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putString("tab", mTabHost.getCurrentTabTag());
+	protected void onCreateTabs() {
+		addTab("albums", "Albums", AlbumsFragment.class, R.drawable.tab_ic_album);
+		addTab("artists", "Artists", AlbumsFragment.class, R.drawable.tab_ic_artist);
+		addTab("files", "Files", AlbumsFragment.class, R.drawable.tab_ic_folder);
 	}
 }
