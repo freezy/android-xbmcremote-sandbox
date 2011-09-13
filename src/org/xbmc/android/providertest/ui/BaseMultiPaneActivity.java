ff4ebb9b06f321f0a0edd4f3ae3c7596bda61ccd
freezy <freezy@xbmc.org>
2011-09-13 22:10:11 +0200

    Added: Tab scrolling, looking good now.

--- src/org/xbmc/android/providertest/ui/BaseMultiPaneActivity.java
+++ src/org/xbmc/android/providertest/ui/BaseMultiPaneActivity.java
@@ -16,6 +16,8 @@
 
 package org.xbmc.android.providertest.ui;
 
+import java.util.List;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -23,8 +25,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-
-import java.util.List;
 
 /**
  * A {@link BaseActivity} that can contain multiple panes, and has the ability to substitute
--- src/org/xbmc/android/providertest/ui/MusicPagerFragment.java
+++ src/org/xbmc/android/providertest/ui/MusicPagerFragment.java
@@ -3,13 +3,13 @@ package org.xbmc.android.providertest.ui;
 import org.xbmc.android.providertest.R;
 import org.xbmc.android.util.TabsAdapter;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.HorizontalScrollView;
 import android.widget.TabHost;
 import android.widget.TabWidget;
 import android.widget.TextView;
@@ -24,16 +24,13 @@ public class MusicPagerFragment extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.fragment_tabs_pager);
+		
 		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+		final HorizontalScrollView scroller = (HorizontalScrollView) findViewById(R.id.tab_scroller);
 		final TabWidget tabWidget = (TabWidget) findViewById(android.R.id.tabs);
 		mTabHost.setup();
 
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		
-		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab, tabWidget, false);
-		TextView title = (TextView) tabIndicator.findViewById(R.id.title);
-		title.setText("testest");
-
+		mViewPager = (ViewPager) findViewById(R.id.tab_pager);
 		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
 
 		addTab("albums", "Albums", AlbumsFragment.class, tabWidget);
@@ -41,16 +38,28 @@ public class MusicPagerFragment extends FragmentActivity {
 		addTab("genres", "Genres", AlbumsFragment.class, tabWidget);
 		addTab("compilations", "Compilations", AlbumsFragment.class, tabWidget);
 		addTab("test3", "FOOOBAR", AlbumsFragment.class, tabWidget);
-
+		
+		
+		mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+			@Override
+			public void onTabChanged(String tabId) {
+				final int position = mTabHost.getCurrentTab();
+				final View tab = tabWidget.getChildTabViewAt(position);
+				scroller.smoothScrollTo(tab.getLeft(), 0);
+				mViewPager.setCurrentItem(position);
+			}
+		});
 
 		if (savedInstanceState != null) {
 			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
 		}
 	}
 	
+
+	
 	private void addTab(String tabspec, String label, Class<?> c, TabWidget tabWidget) {
 		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab, tabWidget, false);
-		TextView title = (TextView) tabIndicator.findViewById(R.id.title);
+		TextView title = (TextView) tabIndicator.findViewById(R.id.tab_title);
 		title.setText(label);
 		//ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
 		mTabsAdapter.addTab(mTabHost.newTabSpec(tabspec).setIndicator(tabIndicator), label, c, null);
