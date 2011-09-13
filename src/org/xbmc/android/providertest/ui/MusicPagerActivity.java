42accf0720aca4a4eb4bc10a41251e119e02d720
freezy <freezy@xbmc.org>
2011-09-13 22:43:37 +0200

    Added: Tab icons for artists and files.

--- src/org/xbmc/android/providertest/ui/MusicPagerActivity.java
+++ src/org/xbmc/android/providertest/ui/MusicPagerActivity.java
@@ -6,15 +6,15 @@ import org.xbmc.android.util.TabsAdapter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabWidget;
 import android.widget.TextView;
 
-public class MusicPagerFragment extends FragmentActivity {
+public class MusicPagerActivity extends FragmentActivity {
 	TabHost mTabHost;
 	ViewPager mViewPager;
 	TabsAdapter mTabsAdapter;
@@ -33,11 +33,9 @@ public class MusicPagerFragment extends FragmentActivity {
 		mViewPager = (ViewPager) findViewById(R.id.tab_pager);
 		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
 
-		addTab("albums", "Albums", AlbumsFragment.class, tabWidget);
-		addTab("artists", "Artists", AlbumsFragment.class, tabWidget);
-		addTab("genres", "Genres", AlbumsFragment.class, tabWidget);
-		addTab("compilations", "Compilations", AlbumsFragment.class, tabWidget);
-		addTab("test3", "FOOOBAR", AlbumsFragment.class, tabWidget);
+		addTab(tabWidget, "albums", "Albums", AlbumsFragment.class, R.drawable.tab_ic_album);
+		addTab(tabWidget, "artists", "Artists", AlbumsFragment.class, R.drawable.tab_ic_artist);
+		addTab(tabWidget, "genres", "Genres", AlbumsFragment.class, R.drawable.tab_ic_folder);
 		
 		
 		mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
@@ -55,13 +53,12 @@ public class MusicPagerFragment extends FragmentActivity {
 		}
 	}
 	
-
-	
-	private void addTab(String tabspec, String label, Class<?> c, TabWidget tabWidget) {
-		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab, tabWidget, false);
-		TextView title = (TextView) tabIndicator.findViewById(R.id.tab_title);
+	private void addTab(TabWidget tabWidget, String tabspec, String label, Class<?> c, int imgRes) {
+		final View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab, tabWidget, false);
+		final TextView title = (TextView) tabIndicator.findViewById(R.id.tab_title);
+		final ImageView icon = (ImageView) tabIndicator.findViewById(R.id.tab_icon);
 		title.setText(label);
-		//ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
+		icon.setImageResource(imgRes);
 		mTabsAdapter.addTab(mTabHost.newTabSpec(tabspec).setIndicator(tabIndicator), label, c, null);
 	}
 
