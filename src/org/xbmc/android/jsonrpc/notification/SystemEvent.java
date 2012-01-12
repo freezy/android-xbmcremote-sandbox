2a6e4f1b29f54a26f21f28482858d92970c00912
freezy <freezy@xbmc.org>
2012-01-12 13:27:39 +0100

    Added: Parsing for System notifications.

---
+++ src/org/xbmc/android/jsonrpc/notification/SystemEvent.java
@@ -0,0 +1,93 @@
+/*
+ *      Copyright (C) 2005-2015 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC Remote; see the file license.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+package org.xbmc.android.jsonrpc.notification;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Parses System.* events.
+ * 
+ * @author freezy <freezy@xbmc.org>
+ */
+public class SystemEvent {
+	
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+	 *  notifications: https://github.com/xbmc/xbmc/blob/master/xbmc/interfaces/json-rpc/notifications.json
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	/**
+	 * XBMC will be closed.
+	 */
+	public static class OnQuit extends AbstractEvent {
+		public final static String METHOD = "System.OnQuit";
+		public OnQuit(JSONObject obj) throws JSONException {
+			super(obj);
+		}
+		@Override
+		public String toString() {
+			return 	"QUIT";
+		}
+	}
+	
+	/**
+	 * The system will be restarted.
+	 */
+	public static class OnRestart extends AbstractEvent {
+		public final static String METHOD = "System.OnRestart";
+		public OnRestart(JSONObject obj) throws JSONException {
+			super(obj);
+		}
+		@Override
+		public String toString() {
+			return 	"RESTART";
+		}
+	}
+	
+	/**
+	 * The system woke up from suspension.
+	 */
+	public static class OnWake extends AbstractEvent {
+		public final static String METHOD = "System.OnWake";
+		public OnWake(JSONObject obj) throws JSONException {
+			super(obj);
+		}
+		@Override
+		public String toString() {
+			return 	"WAKEUP";
+		}
+	}
+	
+	/**
+	 * The system woke up from suspension.
+	 */
+	public static class OnLowBattery extends AbstractEvent {
+		public final static String METHOD = "System.OnLowBattery";
+		public OnLowBattery(JSONObject obj) throws JSONException {
+			super(obj);
+		}
+		@Override
+		public String toString() {
+			return 	"LOWBATTERY";
+		}
+	}
+}
