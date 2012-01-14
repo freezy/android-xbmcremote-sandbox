90443a17c1e7e01a3c8aba5f04425c0b0cbe0cb9
Joel Stemmer <stemmertech@gmail.com>
2012-01-14 22:09:35 +0100

    Add unit test project
    Test project was created in tests/ as described in the android testing
    fundamentals.
    http://developer.android.com/guide/topics/testing/testing_android.html
    
    To compile the test project run `ant clean debug`. Then install and run
    the tests with `ant installt test`.

---
+++ tests/src/org/xbmc/android/test/ExampleTest.java
@@ -0,0 +1,9 @@
+package org.xbmc.android.test;
+
+import junit.framework.TestCase;
+
+public class ExampleTest extends TestCase {
+	public void testExample1() throws Exception {
+		assertTrue(true);
+	}
+}
