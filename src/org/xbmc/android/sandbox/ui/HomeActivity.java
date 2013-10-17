ca1de42374cc412300763a77d1de5c8b693e8048
freezy <freezy@xbmc.org>
2013-10-17 22:44:21 +0200

    Added ABS as project dep.

---
+++ src/org/xbmc/android/sandbox/ui/HomeActivity.java
@@ -0,0 +1,11 @@
+package org.xbmc.android.sandbox.ui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neh
+ * Date: 17.10.13
+ * Time: 22:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomeActivity {
+}
