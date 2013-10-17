b7daefcb4caeef92a02f9606eda7d073341f4d7e
freezy <freezy@xbmc.org>
2013-10-17 23:53:50 +0200

    Added new base activity.

---
+++ src/org/xbmc/android/sandbox/ui/BaseActivity.java
@@ -0,0 +1,71 @@
+package org.xbmc.android.sandbox.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import org.xbmc.android.remotesandbox.R;
+
+/**
+ * Common to all fragment activities.
+ *
+ * @author freezy <freezy@xbmc.org>
+ */
+public class BaseActivity extends SlidingFragmentActivity {
+
+	private int mTitleRes;
+	protected ListFragment mFrag;
+
+	public BaseActivity(int titleRes) {
+		mTitleRes = titleRes;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setTitle(mTitleRes);
+
+		// set the Behind View
+		setBehindContentView(R.layout.menu_frame);
+		if (savedInstanceState == null) {
+			FragmentTransaction t = this.getSupportFragmentManager().beginTransaction();
+			//mFrag = new SampleListFragment();
+			t.replace(R.id.menu_frame, mFrag);
+			t.commit();
+		} else {
+			mFrag = (ListFragment)this.getSupportFragmentManager().findFragmentById(R.id.menu_frame);
+		}
+
+		// customize the SlidingMenu
+		SlidingMenu sm = getSlidingMenu();
+		sm.setShadowWidthRes(R.dimen.slidingmenu_shadow_width);
+		sm.setShadowDrawable(R.drawable.slidingmenu_shadow);
+		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		sm.setFadeDegree(0.35f);
+		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+		//getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				toggle();
+				return true;
+/*			case R.id.github:
+				Util.goToGitHub(this);
+				return true;*/
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
--- src/org/xbmc/android/sandbox/ui/HomeActivity.java
+++ src/org/xbmc/android/sandbox/ui/HomeActivity.java
@@ -2,17 +2,32 @@ package org.xbmc.android.sandbox.ui;
 
 import android.os.Bundle;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import org.xbmc.android.remotesandbox.R;
 
 /**
  * The landing page of the app.
  *
  * @author freezy <freezy@xbmc.org>
  */
-public class HomeActivity extends SlidingFragmentActivity {
+public class HomeActivity extends BaseActivity {
+
+	public HomeActivity() {
+		super(R.string.app_name);
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// set the Above View
+		setContentView(R.layout.activity_home);
+//		setBehindContentView(R.layout.slidingmenu);
+/*		getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.content_frame, new SampleListFragment())
+				.commit();
+*/
+//		setSlidingActionBarEnabled(false);
 
 	}
 }
