0f10fd7b397944d1d9156e7bfa8ac1d04b16e77b
freezy <freezy@xbmc.org>
2013-12-09 17:12:48 +0100

    Added abstract wizard page.

---
+++ app/src/main/java/org/xbmc/android/account/authenticator/ui/AbstractWizardFragment.java
@@ -0,0 +1,63 @@
+package org.xbmc.android.account.authenticator.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A parent class for all wizard fragments.
+ *
+ * @author freezy <freezy@xbmc.org>
+ */
+public abstract class AbstractWizardFragment extends Fragment {
+
+	protected final static int STATUS_ENABLED = 0x01;
+	protected final static int STATUS_DISABLED = 0x02;
+	protected final static int STATUS_GONE = 0x03;
+
+	private final int layoutRes;
+
+	protected AbstractWizardFragment(int layoutRes) {
+		this.layoutRes = layoutRes;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(layoutRes, container);
+	}
+
+	/**
+	 * Indicates whether a page has a next page.
+	 * @return one of: STATUS_ENABLED, STATUS_DISABLED or STATUS_GONE.
+	 */
+	abstract int hasNext();
+	/**
+	 * Indicates whether a page has a previous page.
+	 * @return one of: STATUS_ENABLED, STATUS_DISABLED or STATUS_GONE.
+	 */
+	abstract int hasPrev();
+
+	/**
+	 * Returns the next page.
+	 * @return Next page
+	 */
+	AbstractWizardFragment getNext() {
+		return null;
+	}
+
+	/**
+	 * Returns the previous page.
+	 * @return Previous page
+	 */
+	AbstractWizardFragment getPrev() {
+		return null;
+	}
+
+	/**
+	 * Called when status has changed and next/prev potentially return a different result.
+	 */
+	void onStatusChanged() {
+	}
+}
--- app/src/main/java/org/xbmc/android/account/authenticator/ui/Step1WelcomeFragment.java
+++ app/src/main/java/org/xbmc/android/account/authenticator/ui/Step1WelcomeFragment.java
@@ -1,16 +1,35 @@
 package org.xbmc.android.account.authenticator.ui;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import org.xbmc.android.remotesandbox.R;
 
-public class Step1WelcomeFragment extends Fragment {
+public class Step1WelcomeFragment extends AbstractWizardFragment {
+
+	protected Step1WelcomeFragment() {
+		super(R.layout.fragment_auth_wizard_01_welcome);
+	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_auth_wizard_01_welcome, container, false);
+	int hasNext() {
+		return STATUS_ENABLED;
+	}
+
+	@Override
+	int hasPrev() {
+		return STATUS_GONE;
+	}
+
+	@Override
+	AbstractWizardFragment getNext() {
+		return null;
+	}
+
+	@Override
+	AbstractWizardFragment getPrev() {
+		return null;
+	}
+
+	@Override
+	void onStatusChanged() {
+
 	}
 }
