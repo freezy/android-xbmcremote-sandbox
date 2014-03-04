e90b8b4f83f56a726c581b3c7d662c0946c303f9
freezy <freezy@xbmc.org>
2014-03-04 23:19:04 +0100

    Added Youtube Player API integration.

--- app/src/main/java/org/xbmc/android/app/ui/MovieActivity.java
+++ app/src/main/java/org/xbmc/android/app/ui/MovieActivity.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.BaseColumns;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -13,8 +14,12 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.InjectView;
 import com.bumptech.glide.Glide;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import org.xbmc.android.app.manager.IconManager;
 import org.xbmc.android.app.manager.ImageManager;
 import org.xbmc.android.app.provider.VideoContract;
@@ -23,16 +28,19 @@ import org.xbmc.android.remotesandbox.R;
 
 import javax.inject.Inject;
 import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author freezy <freezy@xbmc.org>
  */
-public class MovieActivity extends BaseActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MovieActivity extends BaseActivity implements LoaderManager.LoaderCallbacks<Cursor>, YouTubePlayer.OnInitializedListener {
 
 	public static final String EXTRA_MOVIE_ID = "MOVIE_ID";
 
 	private static final String TAG = MovieActivity.class.getSimpleName();
 	private static final DecimalFormat FORMATTER = new DecimalFormat("###,###,###,###");
+	private static final Pattern PATTERN_YOUTUBE = Pattern.compile("plugin://plugin\\.video\\.youtube.*videoid=(\\w+)");
 
 	@InjectView(R.id.title) protected TextView titleView;
 	@InjectView(R.id.rating) protected TextView ratingView;
@@ -48,6 +56,7 @@ public class MovieActivity extends BaseActivity implements LoaderManager.LoaderC
 	@Inject protected IconManager iconManager;
 
 	private Uri uri;
+	private String youtubeId;
 
 	public MovieActivity() {
 		super(R.layout.activity_movie);
@@ -112,6 +121,20 @@ public class MovieActivity extends BaseActivity implements LoaderManager.LoaderC
 			genresView.setText(data.getString(MoviesQuery.GENRES));
 			plotView.setText(data.getString(MoviesQuery.PLOT));
 
+			// trailer
+			final FragmentManager fm = getSupportFragmentManager();
+			final YouTubePlayerSupportFragment youtubeFragment = (YouTubePlayerSupportFragment) fm.findFragmentById(R.id.youtube);
+
+
+			final Matcher matcher = PATTERN_YOUTUBE.matcher(data.getString(MoviesQuery.TRAILER));
+			if (matcher.find()) {
+				youtubeId = matcher.group(1);
+				Log.d(TAG, "Found youtube trailer " + youtubeId);
+				youtubeFragment.initialize("AIzaSyDdKyESE56AQvphuVCU0hgpittw9kWCYwQ", this);
+			} else {
+				fm.beginTransaction().hide(youtubeFragment).commit();
+			}
+
 		} else {
 			Log.e(TAG, "No movie found.");
 		}
@@ -119,6 +142,22 @@ public class MovieActivity extends BaseActivity implements LoaderManager.LoaderC
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
+	}
+
+	@Override
+	public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean b) {
+		Log.d(TAG, "Youtube play initialized.");
+		player.cueVideo(youtubeId);
+	}
+
+	@Override
+	public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+		if (errorReason.isUserRecoverableError()) {
+			errorReason.getErrorDialog(this, 1).show();
+		} else {
+			String errorMessage = String.format(getString(R.string.youtube_error), errorReason.toString());
+			Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+		}
 	}
 
 
@@ -140,7 +179,8 @@ public class MovieActivity extends BaseActivity implements LoaderManager.LoaderC
 				VideoContract.Movies.THUMBNAIL,
 				VideoContract.Movies.FANART,
 				VideoContract.Movies.VOTES,
-				VideoContract.Movies.PLOT
+				VideoContract.Movies.PLOT,
+				VideoContract.Movies.TRAILER
 
 		};
 
@@ -156,6 +196,7 @@ public class MovieActivity extends BaseActivity implements LoaderManager.LoaderC
 		final int FANART = 9;
 		final int VOTES = 10;
 		final int PLOT = 11;
+		final int TRAILER = 12;
 	}
 
     @Override
